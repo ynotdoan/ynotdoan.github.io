830943efde5fefbaf0ddd307fcd8ce9b3d9a2b91
Tony Doan <ynotdoan@gmail.com>
2023-07-12 14:09:52 -0700

    Switched contacts and name

--- site/src/components/Header.jsx
+++ site/src/components/Header.jsx
@@ -12,14 +12,10 @@ function Header() {
         <h1 className="inline text-5xl">Check out my card...</h1>
       </div>
 
+      {/* Card */}
       <div className="flex flex-wrap justify-between items- text-center bg-[#F9F6EE] w-1/2 max-w-[900px] min-w-[300px] aspect-[5/3] drop-shadow-2xl">
-        {/* Site name */}
+        {/* Contact link */}
         <div className="mt-8 ml-6">
-          <h2 className="inline">ynotdoan</h2>
-        </div>
-
-        {/* Contact */}
-        <div className="mt-8 mr-6">
           <a href="https://www.linkedin.com/in/ynotdoan/" target="_blank">
             <img src={linkedin} className="px-1" />
           </a>
@@ -31,13 +27,18 @@ function Header() {
           </a>
         </div>
 
+        {/* Site name */}
+        <div className="mt-8 mr-6">
+          <h2 className="inline">ynotdoan</h2>
+        </div>
+
         {/* Name */}
         <div className="w-full">
           <h1 className="">Tony DOAN</h1>
           <h2 className="">Software Engineer</h2>
         </div>
         
-        {/* Links */}
+        {/* Nav links */}
         <div className="w-full">
           <h2>
             <a>Projects</a> | <a>Experience</a> | <a>About</a>
@@ -45,6 +46,7 @@ function Header() {
         </div>
       </div>
 
+      {/* Resume link */}
       <div>
         <a href={resume} target="_blank">
           <button><h2>View my resume</h2></button>
