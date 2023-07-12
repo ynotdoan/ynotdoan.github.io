40470bbfa06cf08469df11ec455f500c1ed36770
Tony Doan <ynotdoan@gmail.com>
2023-07-12 14:10:17 -0700

    Switched links and name

--- site/src/components/Navbar.jsx
+++ site/src/components/Navbar.jsx
@@ -3,13 +3,13 @@ function Navbar() {
     <div className="bg-white sticky top-0 flex justify-between px-6 border-0 border-b-8 border-solid z-10">
 
       <div>
-        <h1 className="inline">TONY DOAN</h1>
-      </div>
-
-      <div>
         <h2 className="inline"><a href="">Projects</a> :: </h2>
         <h2 className="inline"><a href="">Experience</a> :: </h2>
         <h2 className="inline"><a href="">About</a> </h2>
+      </div>
+
+      <div>
+        <h1 className="inline">TONY DOAN</h1>
       </div>
 
     </div>
